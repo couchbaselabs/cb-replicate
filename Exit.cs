62b962b6e8b9ddf9e0eb4d198c3bee1f8ae81c4a
Zachary Gramana <z.gramana@me.com>
2015-08-12 15:22:41 -0700

    Error handling and cleanup.

---
+++ Exit.cs
@@ -0,0 +1,15 @@
+using System;
+using Mono.Options;
+using System.Collections.Generic;
+using Couchbase.Lite;
+using System.IO;
+using System.Threading;
+using System.Text;
+
+namespace cbreplicate
+{
+	public class Exit
+	{
+	}
+
+}
