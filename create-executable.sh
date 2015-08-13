20d03cca50b216521841406f5c3106b8e0261a42
Zachary Gramana <z.gramana@me.com>
2015-08-13 13:38:34 -0700

    Updates help text. Adds script.
    The help text now accurately reflects the
    ability to pass the CBLite file path
    without '-f'.
    
    Includes a script that will use mkbundle
    to compile a self-contained statically-
    linked executable for OSX.

---
+++ create-executable.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Mac-specific instructions
+export AS="as -arch i386"
+export CC="cc -framework CoreFoundation -lobjc -liconv -arch i386"
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/pkgconfig:/Library/Frameworks/Mono.framework/Versions/Current/lib/pkgconfig
+export LD_LIBRARY_PATH=/Library/Frameworks/Mono.framework/Versions/Current/lib/
+cwd=`pwd`
+cd bin/Release/
+mkbundle  --deps --static -o cb-replicate cbreplicate.exe *.dll
+cd $cwd
