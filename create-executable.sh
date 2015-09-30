2c09344260a252e0b8c37e0d94d52111af644128
Zachary Gramana <z.gramana@me.com>
2015-09-30 13:17:54 -0700

    Added Mono path export to handle multiple Mono installs

--- create-executable.sh
+++ create-executable.sh
@@ -5,7 +5,11 @@ export AS="as -arch i386"
 export CC="cc -framework CoreFoundation -lobjc -liconv -arch i386"
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/pkgconfig:/Library/Frameworks/Mono.framework/Versions/Current/lib/pkgconfig
 export LD_LIBRARY_PATH=/Library/Frameworks/Mono.framework/Versions/Current/lib/
+export MONO_PATH=/Library/Frameworks/Mono.framework/Versions/Current/
+
 cwd=`pwd`
 cd bin/Release/
+
 mkbundle  --deps --static -o cb-replicate cbreplicate.exe *.dll
+
 cd $cwd
